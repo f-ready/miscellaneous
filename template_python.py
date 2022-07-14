b84d7a3a9270bc3f68cfc8879e6851d43bdb2510
Xu, Freddy <xufreddy9@gmail.com>
2022-07-14 15:10:29 -0700

    Updated pandas initialization settings.

--- template_python.py
+++ template_python.py
@@ -19,16 +19,21 @@
 ###########################################################################
 
 import gc, os, sys
-import pandas as pd
-pd.set_option("display.max_rows", None)
-pd.set_option("display.max_columns", None)
-pd.set_option("display.expand_frame_repr", False)
+import matplotlib.pyplot as plt, numpy as np, pandas as pd
 
 
 
 # %% ######################################################################
 ###     1.          Variable Initialization                             ###
 ###########################################################################
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.expand_frame_repr", False)
+pd.set_option("max_colwidth", None)
+pd.set_option("display.float_format", lambda x: "%.5f" % x)
+pd.set_option("use_inf_as_na", True)
+plt.style.use("tableau-colorblind10")
 
 wrk_dir = os.getcwd()
 
@@ -40,4 +45,5 @@ wrk_dir = os.getcwd()
 
 del wrk_dir
 gc.collect()
+
 os.system("cls 2> nul")
