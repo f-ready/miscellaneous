a75cdd090a3881303637acd899cd9380c0a663f5
Xu, Freddy <xufreddy9@gmail.com>
2022-07-14 15:09:12 -0700

    Snowflake procedures with array arguments.

--- template_snowflake.sql
+++ template_snowflake.sql
@@ -58,8 +58,8 @@ create or replace procedure get_cols(
             default:
                 query_result.push(query_return.getColumnValue(3));
                 break;
-            }
-     }
+        }
+    }
     return query_result;
     $$;
 
@@ -89,9 +89,45 @@ create or replace procedure get_tbls(
             default:
                 query_result.push(query_return.getColumnValue(2));
                 break;
-            }
         }
+    }
     return query_result;
+    $$;
+
+create or replace procedure clean_cols(
+    "tbl" string)
+    returns variant
+    language javascript
+    as
+    $$
+    var cmd = null;
+    var stmt = null;
+    var query_return = null;
+    var query_result = null;
+    var cols = [];
+    var col = "";
+    var col_new = "";
+    cmd = `call get_cols('${tbl}', 'exact');`;
+    stmt = snowflake.createStatement({sqlText : cmd});
+    query_return = stmt.execute();
+    query_return.next();
+    query_result = query_return.getColumnValue(1);
+    for (let i = 0; i < query_result.length; i++) {
+        col = query_result[i];
+        if (/\W/.test(col)) {
+            col_new = col.replace(/=/, " equals ");
+            col_new = col_new.replace(/</, " less ");
+            col_new = col_new.replace(/>/, " greater ");
+            col_new = col_new.replace(/\W+/g, "_");
+            if (col_new.slice(-1) == "_") {
+                col_new = col_new.slice(0, -1);
+            }
+            cols.push(`"${col}" as ${col_new}`);
+        } else {
+            cols.pust(col);
+        }
+    }
+    return cols;
     $$;
 
 create or replace procedure find_cols(
@@ -133,7 +169,58 @@ create or replace procedure find_cols(
     return cols_found;
     $$;
 
-call find_cols('data_schema', 'elusive_col');
+--+---------------------------------------------------------------------+--
+
+create or replace procedure add_tbl_ref(
+    "cols" array,
+    "tbl" string)
+    returns variant
+    language javascript
+    as
+    $$
+    var cmd = null;
+    var stmt = null;
+    var query_return = null;
+    var query_result = null;
+    var cols_new = [];
+    var col = "";
+    if (tbl == "") {
+        cols_new = cols;
+    } else {
+        for (let i = 0; i < cols.length; i++) {
+            col = cols[i].trim();
+            cols_new.push(`${tbl}.${col}`);
+        }
+    }
+    return cols_new;
+    $$;
+
+create or replace procedure add_prefix_suffix(
+    "cols" array,
+    "tbl" string,
+    "prefix" string,
+    "suffix" string)
+    returns variant
+    language javascript
+    as
+    $$
+    var cmd = null;
+    var stmt = null;
+    var query_return = null;
+    var query_result = null;
+    var cols_new = [];
+    var col = "";
+    for (let i = 0; i < cols.length; i++) {
+        col = cols[i].trim();
+        cols_new.push(`${col} as ${prefix}${col}${suffix}`);
+    }
+    cmd = `call add_tbl_ref(array_construct('${cols_new.join("', '")}'), '${tbl}');`;
+    stmt = snowflake.createStatement({sqlText : cmd});
+    query_return = stmt.execute();
+    query_return.next();
+    query_result = query_return.getColumnValue(1);
+    return query_result;
+    $$;
 
 
 
@@ -166,10 +253,16 @@ select
 show procedures;
 desc procedure get_cols(varchar, varchar);
 desc procedure get_tbls(varchar, varchar);
+desc procedure clean_cols(varchar);
 desc procedure find_cols(varchar, varchar);
+desc procedure add_tbl_ref(array, varchar);
+desc procedure add_prefix_suffix(array, varchar, varchar, varchar);
 drop procedure if exists get_cols(varchar, varchar);
 drop procedure if exists get_tbls(varchar, varchar);
+drop procedure if exists clean_cols(varchar);
 drop procedure if exists find_cols(varchar, varchar);
+drop procedure if exists add_tbl_ref(array, varchar);
+drop procedure if exists add_prefix_suffix(array, varchar, varchar, varchar);
 show procedures;
 
 --+---------------------------------------------------------------------+--
